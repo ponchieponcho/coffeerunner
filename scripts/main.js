ef20a502c43f4abd99659d36ac485725a1604b9f
Aaron Gross <aarongross@Aarons-MacBook-Air.local>
2018-02-28 15:15:13 -0500

    Added order display

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 var coffeeOrders = [];
 
 var orderForm = document.querySelector('form');
@@ -8,17 +9,43 @@ orderForm.addEventListener('submit', function(event){
     // Get all the values for a coffee order
     var coffeeValue = orderForm.querySelector('[name="coffee"]').value;
     var emailValue = orderForm.querySelector('[name="emailAddress"]').value;
-    var sizeValue = orderForm.querySelector('[name="size"]').value;
+    // var sizeValue = document.querySelector('input[name=size]:checked').value;
     var flavorValue = orderForm.querySelector('[name="flavor"]').value;
     var strValue = orderForm.querySelector('[name="strength"]').value;
 
-    // Pushes all values into one object in array
-    coffeeOrders.push({'coffee':coffeeValue,
-                    'email':emailValue,
-                    'size':sizeValue,
-                    'flavor':flavorValue,
-                    'strength':strValue});
+    function getRadio(rName) {
+        var radioButtons = document.getElementsByName(rName);
+        for (var i=0; i < radioButtons.length; i++) {
+            if (radioButtons[i].checked) 
+                return radioButtons[i].value;
+        }
+        return '';
+    }
 
-    console.log(coffeeOrders);
+   
+    var sizeValue = getRadio('size');
+    console.log(sizeValue);
+
+    // Pushes all values into one object in array
+    var coffeeList = {"coffee":coffeeValue,
+                    "email":emailValue,
+                    "size":sizeValue,
+                    "flavor":flavorValue,
+                    "strength":strValue};
+        
+    coffeeOrders.push(coffeeList);
+    var newDiv = document.createElement('div');
+    var newUl = document.createElement('ul');
+    var section = document.querySelector('section');
+    var display1Array = JSON.stringify(coffeeList.coffee, null, 4);
+    var display2Array = JSON.stringify(coffeeList.email, null, 4);
+    var display3Array = JSON.stringify(coffeeList.size, null, 4);
+    var display4Array = JSON.stringify(coffeeList.flavor, null, 4);
+    var display5Array = JSON.stringify(coffeeList.strength, null, 4);
+    newUl.innerHTML = 'Coffee Order: '+ display1Array + '<br>' + 'Email: ' + display2Array + '<br>' + 'Size: ' + display3Array + '<br>' + 'Flavor Shot: ' + display4Array+ '<br>' + 'Caffeine Rating: ' + display5Array;
+   
+    section.appendChild(newDiv);
+    newDiv.appendChild(newUl);
 
 });
+
